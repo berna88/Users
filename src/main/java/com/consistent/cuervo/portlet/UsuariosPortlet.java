e387cb6f07186a8de9d5ae506d1f132187fc2d7e
Bernardo <berna_halo@hotmail.com>
2019-09-17 18:35:43 -0500

    ajustede portlet

--- src/main/java/com/consistent/cuervo/portlet/UsuariosPortlet.java
+++ src/main/java/com/consistent/cuervo/portlet/UsuariosPortlet.java
@@ -5,21 +5,14 @@ import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.LayoutSet;
-import com.liferay.portal.kernel.model.Organization;
-import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.model.UserGroup;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
-import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
-import com.liferay.portal.kernel.service.LayoutSetLocalServiceUtil;
-import com.liferay.portal.kernel.service.OrganizationLocalServiceUtil;
-import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
 import com.liferay.portal.kernel.service.UserGroupLocalServiceUtil;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 
 import java.io.IOException;
 import java.util.List;
@@ -33,101 +26,100 @@ import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
 
-
 /**
  * @author bernardohernandez
  */
-@Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=category.sample",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.name=" + UsuariosPortletKeys.Usuarios,
+@Component(immediate = true, property = { "com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true", "javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp", "javax.portlet.name=" + UsuariosPortletKeys.Usuarios,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class
-)
+		"javax.portlet.security-role-ref=power-user,user" }, service = Portlet.class)
 public class UsuariosPortlet extends MVCPortlet {
-	
+
 	private static final Log log = LogFactoryUtil.getLog(UsuariosPortlet.class);
-	
+
 	@Override
-		public void render(RenderRequest arg0, RenderResponse arg1) throws IOException, PortletException {
-			// TODO Auto-generated method stub
-		List<Organization> organizations = OrganizationLocalServiceUtil.getOrganizations(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-		arg0.setAttribute("Logos", organizations);
-			super.render(arg0, arg1);
-		}
+	public void render(RenderRequest arg0, RenderResponse arg1) throws IOException, PortletException {
+		// TODO Auto-generated method stub
+		
+		super.render(arg0, arg1);
+	}
 	
-	
+
 	public void convenio(ActionRequest request, ActionResponse response) throws PortalException {
 		log.info("Metodo convenio");
-		List<Organization> organizations = OrganizationLocalServiceUtil.getOrganizations(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-		for (Organization organization : organizations) {
-			log.info("<-- inicio -->");
-			log.info("Logo: "+organization.getLogoId());
-			log.info(organization.getOrganizationId());
-			log.info(organization.getName());
-			List<User> users = UserLocalServiceUtil.getOrganizationUsers(organization.getOrganizationId());
-			for (User user : users) {
-				log.info("Nombre"+user.getFullName());
-			}
-			log.info("<-- Fin -->");
-		}
+		ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
 		
-		final long companyIds = PortalUtil.getDefaultCompanyId();
-		Long organizationId = (long) 34328;
-		Group group = GroupLocalServiceUtil.getOrganizationGroup(companyIds, organizationId);
-		LayoutSet layoutSet = LayoutSetLocalServiceUtil.getLayoutSet(group.getGroupId(), true);
-		if(layoutSet.isLogo()) {
-			
-			log.info("Live logo"+layoutSet.getLiveLogoId());
-			log.info("Live logoId"+layoutSet.getLogoId());
-		}else {
-			log.info(
-                    "private community does  not have logo");
-		}
+		String destino = (String)themeDisplay.getSiteGroup().getExpandoBridge().getAttribute("convenios");
+		String[] arryDest = destino.split(",");
+		log.info(destino);
+		log.info(arryDest.length);
 		
-		List<User> users2 = UserLocalServiceUtil.getOrganizationUsers(organizationId);
-		for (User user : users2) {
-			log.info("user"+user.getFullName());
-		}
-		if(ParamUtil.getString(request, "Convenio")!=null && !ParamUtil.getString(request, "Convenio").isEmpty()) {
+		if (ParamUtil.getString(request, "Convenio") != null && !ParamUtil.getString(request, "Convenio").isEmpty()) {
 			final String convenio = ParamUtil.getString(request, "Convenio");
-			log.info("Tipo de grupo: "+convenio);
+			log.info("Tipo de grupo: " + convenio);
 			try {
-				final String groupName = "Desarrolladores";
 				final long companyId = PortalUtil.getDefaultCompanyId();
-				
-				System.out.println(UserGroupLocalServiceUtil.getUserGroup(companyId, groupName).getName());
-				System.out.println(UserGroupLocalServiceUtil.getUserGroup(companyId, groupName).getGroupId());
-				//List<Group> groups = GroupLocalServiceUtil.getGroups(0,GroupLocalServiceUtil.getGroupsCount());
+
 				List<User> users = UserLocalServiceUtil.getUsers(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
 				for (User user : users) {
-					if(user!=null) {
-							if(user.isActive()) {
-								if(!user.isDefaultUser()) {
-									System.out.println(user.getFullName());
-								}
-							}
-							//UserLocalServiceUtil.addUserGroupUser(UserGroupLocalServiceUtil.getUserGroup(companyId, groupName).getUserGroupId(), user.getUserId());
+					if (user != null) {
+						try {
+							log.info("Nombre de usuario: " + user.getFullName());
+							Long groupUserId = UserGroupLocalServiceUtil.getUserGroup(companyId, validConvenio(user.getExpandoBridge().getAttribute("Clave_Convenio").toString())).getUserGroupId();
+							log.info(groupUserId);
+							UserLocalServiceUtil.addUserGroupUser(groupUserId, user.getUserId());
+						} catch (Exception e) {
+							// TODO: handle exception
+							log.info("No tiene convenio");
+							e.getMessage();
+							
+						}
+								
+								
+						
+						 
 					}
 				}
 			} catch (Exception e) {
+				e.getMessage();
+				e.printStackTrace();
 				// TODO: handle exception
 			}
-		}else {
+		} else {
 			log.info("No tiene valor");
 		}
-		
-		
+
 	}
-	
+
 	private String validConvenio(String convenio) {
-		
+		switch (convenio) {
+		case "1":
+			return "EJECUTIVOS";
+		case "2":
+			return "NO SINDICALIZADOS";
+		case "3":
+			return "PROMOTORES";
+		case "5":
+			return "SINDICALIZADOS LA LAJA";
+		case "6":
+			return "SINDICALIZADOS EVENTUALES EDISA";
+		case "7":
+			return "SINDICALIZADOS PLANTA EDISA";
+		case "8":
+			return "SINDICALIZADOS LA ROJEÑA";
+		case "9":
+			return "JUBILADOS LA ROJEÑA";
+		case "11":
+			return "EJECUTIVOS B";
+		case "12":
+			return "NO SINDICALIZADOS B";
+		case "13":
+			return "SEGURIDAD CORPORATIVA";
+		default:
+			break;
+		}
+
 		return "";
 	}
 }
